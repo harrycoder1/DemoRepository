e2b671f055acc38168a4af65cac0feae9f3a6a19
harycoder1 <harish7823@gmail.com>
2022-10-25 09:41:58 +0530

    Initial commit

---
+++ importent files/in.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+}[D
+[A
+[A[pritnf(""[Dhelllo[2~ ");
+return 0 ;
+}
+printf("");[D[D[Dthis haping in our life");
+pruinb
+dfjksdfjkldfljkd
+dsjksdjkdjk
+jdshj
